d73b253db2602d9339a5734f3922a7a2a3732661
Abhishek <abhisheksangule6@gmail.com>
2025-07-22 10:55:58 +0530

    setup the redux and state in authSlice and store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 
 import './App.css'
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Header, Footer } from './components';
+import AuthClassObject from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APP_APPWRITE_URL)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <div>hello</div>
-  )
+  useEffect(() => {
+    AuthClassObject.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }))
+      } else {
+        dispatch(logout())
+      }
+    }).catch((erro) => {
+      console.log("Error fetching user data:", erro);
+    }).finally(() => {
+      setLoading(false);
+    })
+  }, [])
+
+  return !loading ? (
+    <div className='bg-amber-200 min-h-screen'>
+      <div>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
